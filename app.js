2a578c219fa0665dfa7e81360785980d9f41a4e0
Michael Nickey <mnickey@gmail.com>
2016-10-10 18:11:31 -0700

    resolving clean text call not being executed. Added js-form to html

--- app.js
+++ app.js
@@ -5,22 +5,24 @@ function report(text) {
 
 // clean entered text
 function cleanText(text) {
+    console.log("Here I am");
     return text.replace(/\r?\n|\r/g, "");
 }
 
 // watch form submission
 function submitForm() {
+    console.log("In the submit form function");
     $('.js-form').submit(function(event) {
         event.preventDefault();
         // get text
         var enteredText = $(this).find('#user-text').value();
-        cleanText(enteredText);
+        enteredText = cleanText(enteredText);
         report(enteredText);
     });
 }
 
 // initalize the document
 $( document ).ready(function() {
-    // console.log( "ready!" );
+    console.log( "ready!" );
     submitForm();
 });
